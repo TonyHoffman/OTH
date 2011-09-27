520876e3859bd2a490c3af9005120f7d810c2964
Tony Hoffman <thetonyhoffman@gmail.com>
2011-09-27 11:44:13 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OTH::Application.config.secret_token = 'e197213fc42890494c0c6eb79705881a5d03cb70f7100e073abbe2de14bcd2f84292193848ac9d49273f1de0b0518d30df3f6e45de872c47faae4aa07a3053a6'
